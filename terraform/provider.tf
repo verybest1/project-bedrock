01e0c49fe86357f4251a1ef1d94e8f2ede6a5afc
verybest1 <victorverybest@gmail.com>
2026-08-16 21:32:24 +0100

    Configure remote S3 backend for Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket = "bedrock-terraform-state-090686622011"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
