b3365023048c1d2a9d621f5b1962183f3187f57c
verybest1 <victorverybest@gmail.com>
2026-09-04 01:58:05 +0100

    Map root account principal ARN for GitHub Actions pipeline EKS access

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -116,10 +116,16 @@ resource "aws_eks_access_policy_association" "admin_user_policy" {
   }
 }
 
+resource "aws_eks_access_entry" "pipeline_access" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::878359978914:root"
+  type          = "STANDARD"
+}
+
 resource "aws_eks_access_policy_association" "pipeline_admin" {
   cluster_name  = aws_eks_cluster.main.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = aws_iam_user.dev_user.arn
+  principal_arn = aws_eks_access_entry.pipeline_access.principal_arn
 
   access_scope {
     type = "cluster"
