18eb70c97a3ac355a421c4f6a5889d7864c23c7a
verybest1 <victorverybest@gmail.com>
2026-09-04 02:00:40 +0100

    Grant AmazonEKSClusterAdminPolicy to dev_user for pipeline deployments

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -92,7 +92,7 @@ resource "aws_eks_access_entry" "dev_entry" {
 
 resource "aws_eks_access_policy_association" "dev_policy" {
   cluster_name  = aws_eks_cluster.main.name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = aws_iam_user.dev_user.arn
 
   access_scope {
@@ -110,22 +110,6 @@ resource "aws_eks_access_policy_association" "admin_user_policy" {
   cluster_name  = aws_eks_cluster.main.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = aws_eks_access_entry.admin_user_entry.principal_arn
-
-  access_scope {
-    type = "cluster"
-  }
-}
-
-resource "aws_eks_access_entry" "pipeline_access" {
-  cluster_name  = aws_eks_cluster.main.name
-  principal_arn = "arn:aws:iam::878359978914:root"
-  type          = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "pipeline_admin" {
-  cluster_name  = aws_eks_cluster.main.name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = aws_eks_access_entry.pipeline_access.principal_arn
 
   access_scope {
     type = "cluster"
